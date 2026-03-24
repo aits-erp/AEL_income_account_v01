a03c25695e357546224b7f112cae62e4a427e432
uNik020 <shekharnikhil020@gmail.com>
2026-03-24 18:23:20 +0000

    third commit

--- sales_invoice_account_mapper/api.py
+++ sales_invoice_account_mapper/api.py
@@ -1,6 +1,5 @@
 import frappe
 
-
 @frappe.whitelist()
 def get_item_income_account(item_code, company):
     if not item_code or not company:
@@ -8,13 +7,13 @@ def get_item_income_account(item_code, company):
 
     item = frappe.get_doc("Item", item_code)
 
-    # 1. Check item defaults for matching company
+    # 1. Item Default
     for row in item.get("item_defaults") or []:
         if row.company == company and row.income_account:
             return row.income_account
 
-    # 2. Optional fallback: Item Group default
-    item_group = frappe.db.get_value("Item", item_code, "item_group")
+    # 2. Item Group Default
+    item_group = item.item_group
     if item_group:
         income_account = frappe.db.get_value(
             "Item Default",
@@ -24,13 +23,42 @@ def get_item_income_account(item_code, company):
         if income_account:
             return income_account
 
-    # 3. Optional fallback: Company default income account
-    company_default_income_account = frappe.db.get_value(
-        "Company",
-        company,
-        "default_income_account"
-    )
-    if company_default_income_account:
-        return company_default_income_account
+    # 3. Company Default
+    return frappe.db.get_value("Company", company, "default_income_account") or ""
 
-    return ""
+# import frappe
+
+
+# @frappe.whitelist()
+# def get_item_income_account(item_code, company):
+#     if not item_code or not company:
+#         return ""
+
+#     item = frappe.get_doc("Item", item_code)
+
+#     # 1. Check item defaults for matching company
+#     for row in item.get("item_defaults") or []:
+#         if row.company == company and row.income_account:
+#             return row.income_account
+
+#     # 2. Optional fallback: Item Group default
+#     item_group = frappe.db.get_value("Item", item_code, "item_group")
+#     if item_group:
+#         income_account = frappe.db.get_value(
+#             "Item Default",
+#             {"parent": item_group, "company": company},
+#             "income_account"
+#         )
+#         if income_account:
+#             return income_account
+
+#     # 3. Optional fallback: Company default income account
+#     company_default_income_account = frappe.db.get_value(
+#         "Company",
+#         company,
+#         "default_income_account"
+#     )
+#     if company_default_income_account:
+#         return company_default_income_account
+
+#     return ""
--- sales_invoice_account_mapper/public/js/sales_invoice.js
+++ sales_invoice_account_mapper/public/js/sales_invoice.js
@@ -1,44 +1,65 @@
-frappe.ui.form.on("Sales Invoice", {
-    refresh(frm) {
-        map_income_account_for_all_rows(frm);
-    },
-
-    company(frm) {
-        map_income_account_for_all_rows(frm);
-    }
-});
-
 frappe.ui.form.on("Sales Invoice Item", {
     item_code(frm, cdt, cdn) {
-        set_income_account_in_row(frm, cdt, cdn);
+        let row = locals[cdt][cdn];
+
+        if (!row.item_code || !frm.doc.company) return;
+
+        frappe.call({
+            method: "sales_invoice_account_mapper.api.get_item_income_account",
+            args: {
+                item_code: row.item_code,
+                company: frm.doc.company
+            },
+            callback: function (r) {
+                if (r.message) {
+                    frappe.model.set_value(cdt, cdn, "income_account", r.message);
+                }
+            }
+        });
     }
 });
 
-function map_income_account_for_all_rows(frm) {
-    if (!frm.doc.items || !frm.doc.items.length) return;
+// frappe.ui.form.on("Sales Invoice", {
+//     refresh(frm) {
+//         map_income_account_for_all_rows(frm);
+//     },
 
-    frm.doc.items.forEach(row => {
-        if (row.item_code) {
-            set_income_account_in_row(frm, row.doctype, row.name);
-        }
-    });
-}
+//     company(frm) {
+//         map_income_account_for_all_rows(frm);
+//     }
+// });
 
-function set_income_account_in_row(frm, cdt, cdn) {
-    let row = locals[cdt][cdn];
+// frappe.ui.form.on("Sales Invoice Item", {
+//     item_code(frm, cdt, cdn) {
+//         set_income_account_in_row(frm, cdt, cdn);
+//     }
+// });
 
-    if (!row.item_code || !frm.doc.company) return;
+// function map_income_account_for_all_rows(frm) {
+//     if (!frm.doc.items || !frm.doc.items.length) return;
 
-    frappe.call({
-        method: "sales_invoice_account_mapper.api.get_item_income_account",
-        args: {
-            item_code: row.item_code,
-            company: frm.doc.company
-        },
-        callback: function (r) {
-            if (r.message) {
-                frappe.model.set_value(cdt, cdn, "income_account", r.message);
-            }
-        }
-    });
-}
+//     frm.doc.items.forEach(row => {
+//         if (row.item_code) {
+//             set_income_account_in_row(frm, row.doctype, row.name);
+//         }
+//     });
+// }
+
+// function set_income_account_in_row(frm, cdt, cdn) {
+//     let row = locals[cdt][cdn];
+
+//     if (!row.item_code || !frm.doc.company) return;
+
+//     frappe.call({
+//         method: "sales_invoice_account_mapper.api.get_item_income_account",
+//         args: {
+//             item_code: row.item_code,
+//             company: frm.doc.company
+//         },
+//         callback: function (r) {
+//             if (r.message) {
+//                 frappe.model.set_value(cdt, cdn, "income_account", r.message);
+//             }
+//         }
+//     });
+// }
